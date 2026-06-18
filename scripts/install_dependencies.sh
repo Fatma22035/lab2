77fd3198f09dacf90217cecf8d1b267de8656ea2
Fatma22035 <22035@supnum.mr>
2026-06-18 21:40:25 +0000

    edit istall  dependencies

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,2 @@
 #!/bin/bash
-# Installer Java si nécessaire
-if ! command -v java &> /dev/null; then
-    yum install -y java-1.8.0-amazon-corretto-devel
-fi
-
-# Vérifier Tomcat
-if [ ! -d "/opt/tomcat" ]; then
-    cd /home/ec2-user
-    wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.98/bin/apache-tomcat-9.0.98.tar.gz
-    tar -xzf apache-tomcat-9.0.98.tar.gz
-    mv apache-tomcat-9.0.98 /opt/tomcat
-    chmod +x /opt/tomcat/bin/*.sh
-fi
+echo "No extra dependencies required on EC2"
